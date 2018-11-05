3fc54787d9f0279a7774bb601836b187fee80ee4
tttiff <charles.tiffany.r@gmail.com>
2018-11-05 19:00:11 +0000

    done

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,26 +1,17 @@
 class QuestionsController < ApplicationController
   def ask
-    puts "Hello, I am your coach, what did you want to tell me?"
-
-    answer = nil
-
-    while answer != ""
-      print "> "
-      message = gets.chomp
-      answer = coach_answer_enhanced(message)
-      puts answer
-    end
-
-    puts "Great, see ya!"
   end
 
   def answer
-    if your_message.downcase == "i am going to work right now!"
-      "Great!"
-    elsif your_message.end_with?("?")
-      "Silly question, get dressed and go to work!"
+    @question = params[:question]
+    if @question.blank?
+      @answer = "I can't hear you!"
+    elsif @question.downcase =~ /i am going to work/i
+      @answer = "Great!"
+    elsif @question.end_with?("?")
+      @answer = "Silly question, get dressed and go to work!"
     else
-      "I don't care, get dressed and go to work!"
+      @answer = "I don't care, get dressed and go to work!"
     end
   end
 end
